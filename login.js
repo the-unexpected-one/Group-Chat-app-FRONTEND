54e2778e21b77ae47fcfbc76a3595e968054d0e6
Saloni <saloni.patil2220@gmail.com>
2022-09-27 13:03:22 +0530

    Group Chat app project task 4-5 FRONTEND

---
+++ login.js
@@ -0,0 +1,35 @@
+function login(event){
+    event.preventDefault();
+    console.log('123')
+    const emailid=event.target.emailid.value;
+    const password=event.target.password.value;
+    const obj={
+        emailid,
+        password
+    }
+    axios.post("http://localhost:8000/login",obj)
+    .then((res)=>{
+
+        alert(res.data.message)
+        const token=res.data.token;
+        localStorage.setItem('token',token);
+
+        // location.replace('/addexpense.html')
+        console.log(res)
+    }).catch(err=>{
+
+        console.log(err)
+
+        if(err.response.status==401){
+
+            alert(err.response.data.message)
+        }
+        else if(err.response.status==404){
+            alert(err.response.data.message)
+        }
+        else{
+            console.log(err)
+        }
+        
+    })
+}
